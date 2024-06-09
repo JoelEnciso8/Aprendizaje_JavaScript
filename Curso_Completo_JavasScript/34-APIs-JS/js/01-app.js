b5569f7072b57441537472e3c6ea1ca962cbeaf5
JoelEnciso8 <joelenciso08@outlook.com>
2024-06-09 13:06:13 -0500

    Apis

--- Curso_Completo_JavasScript/34-APIs-JS/js/01-app.js
+++ Curso_Completo_JavasScript/34-APIs-JS/js/01-app.js
@@ -1,26 +1,33 @@
 // APIs - Aplication Program Interface
 
 // APIs propias de Js 
-
+// Accionamos la Notificacion Alerts
 const notificar = document.querySelector('#notificar');
-const verNotifi = document.querySelector('#verNotificacion');
-
 notificar.addEventListener('click',()=>{ // se manda a llamar el query seleccionado
     Notification // se le pide permiso a la API 
     .requestPermission()
     .then(result =>{
-        console.log('el resultado es ',result); // 
+        console.log('el resultado es ',result); // result en consola 
     })
 });
 
-
 // ver notificacion 
-
-
-verNotifi.addEventListener('click',()=>{
-
-        if (Notification.permission === 'granted') {
-            new Notification('Esta es nueva ');
+const verNotificacion = document.querySelector('#verNotificacion')
+verNotificacion.addEventListener('click',()=>{
+    if (Notification.permission === 'granted') {
+        const notificar = new Notification('Esta es la  notificacion ', {
+            icon: 'img/ccj.png',
+            body: 'Aprendiendo con project reales'
+        });
+        notificar.onclick = function () {
+            window.open('https://www.udemy.com/')
         }
+    }
 
 });
+
+
+
+
+
+
