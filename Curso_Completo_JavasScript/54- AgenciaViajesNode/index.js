49143a30c67427719089a2e21b8df7801b6d0510
JoelEnciso8 <joelenciso08@outlook.com>
2024-07-25 23:12:31 -0500

    nodeproject v2

--- Curso_Completo_JavasScript/54- AgenciaViajesNode/index.js
+++ Curso_Completo_JavasScript/54- AgenciaViajesNode/index.js
@@ -1,43 +1,31 @@
 // extrayedno express del package json
-import express from 'express';
-import path from 'path';
-import router from './routes/index.js';
-
+import express from "express";
+import router from "./routes/index.js";
+ 
+// Llamamos a express para que funcione en nuestro proyecto 
 const app = express();
-
-// Definir puerto con variable de entorno
+ 
+// Definir puerto con variable de entorno, nos asigna node de  manera autimatica, cuando pasemos el proyecto a produccion esta variable de entorno tomara fuerza
 const port = process.env.PORT || 4000;
-
-// Habilitar PUG 
-/*permite utilizar archivos estáticos como plantillas, enviar valores para reemplazar variables dentro de las mismas y transformar estos archivos en páginas HTML que se envían al cliente*/
-/* es importante configurar el directorio base donde se encuentran tus vistas (archivos Pug). Puedes hacer esto con app.set('views', './view'*/ 
-app.set('views', path.join(__dirname, 'views', 'layout'));
-app.set('view engine','pug')
-console.log('Views directory set to:', app.get('views'));
-
-
-
-
-
+ 
+// Habilitar PUG
+app.set("view engine", "pug");
+ 
 // MiddleWare, obteniendo el actual year, next nos ayuda a ir al sgt MiddleWare
-app.use((request, response,next)=>{
-    const year = new Date();
-
-    response.locals.actualYear = year.getFullYear();
-    next();
-})
-
-
-
-
-// Definiendo la carpeta public
-app.use(express.static('public'));
-
-
+app.use((request, response, next) => {
+  const year = new Date();
+ 
+  response.locals.actualYear = year.getFullYear();
+  response.locals.nombresitio = "Agencia de Viajes"
+  return next();
+});
+ 
+// Definiendo la carpeta public usando express 
+app.use(express.static("public"));
+ 
 // agregar router
-app.use('/',router);
-
-
-app.listen(port,()=>{
-    console.log(`El servidor esta funcionando en el puerto${port}`); // variable port debe estar creada para deployment
-})
+app.use("/", router);
+ 
+app.listen(port, () => {
+  console.log(`El servidor esta funcionando en el puerto${port}`); // variable port debe estar creada para deployment
+});
--- Curso_Completo_JavasScript/54- AgenciaViajesNode/routes/index.js
+++ Curso_Completo_JavasScript/54- AgenciaViajesNode/routes/index.js
@@ -2,14 +2,17 @@ import express from 'express';
 
 const router = express.Router();
 
-
+/*express: Soporta los dif verbos de HTTP como son , POST,GET,PUT, PATCH,DELETE, en routes
+permite la creation de app en MVC, soporat middleware , si esta en el pipe line*/  
 // express soporta los verbos de las APIS, PUCH , PATCH , POST
+
     router.get('/',(req,res)=>{ // req-lo eque enviamos, res-lo que express nos resonde
         res.render('inicio',{
             pagina: 'Inicio'
         })
             
     });
+
     router.get('/nosotros',(req,res)=>{ // req-lo eque enviamos, res-lo que express nos resonde
         res.render('nosotros',{
             pagina: 'Nosotros'
